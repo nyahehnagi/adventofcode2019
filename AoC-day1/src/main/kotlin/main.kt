1b50c665b5765128e9044de76a6207815d70e899
bromley.jones <bromley.jones@johnlewis.com>
2019-12-02 16:28:12 +0000

    day 2, part 1 complete

--- AoC-day1/src/main/kotlin/main.kt
+++ AoC-day1/src/main/kotlin/main.kt
@@ -3,9 +3,41 @@ package adventofcode
 import java.io.File
 import kotlin.math.floor
 
+//https://adventofcode.com/2019/day/2
+
 fun main() {
-    println(calculateTotalFuel(getData()))
+    println(calculateTotalFuel(getData("src/main/resources/inputdata.txt")))
+    println(calculateIntCodeFinalState(getData("src/main/resources/day2input.csv")[0]))
 }
+
+fun getData(filename: String): List<String> {
+    val data = File(filename)
+    return data.readLines()
+}
+
+fun calculateIntCodeFinalState(intCode: String): String {
+    val codeList = intCode.split(",").toMutableList()
+
+    for (i in 0..codeList.lastIndex step 4) {
+        if (codeList[i] != "99") {
+            var firstValue = codeList[codeList[i + 1].toInt()].toInt()
+            var secondValue = codeList[codeList[i + 2].toInt()].toInt()
+            var thirdValue = codeList[i + 3].toInt()
+            when {
+                codeList[i].toInt() == 1 ->
+                    codeList[thirdValue] = (firstValue + secondValue).toString()
+                codeList[i].toInt() == 2 ->
+                    codeList[thirdValue] = (firstValue * secondValue).toString()
+                else -> println("Errr")
+            }
+        }
+        println(codeList.joinToString (","))
+    }
+
+    val retVal = codeList.joinToString (",")
+    return retVal
+}
+
 
 fun calculateTotalFuel(data: List<String>): Int {
     var total = 0
@@ -13,11 +45,6 @@ fun calculateTotalFuel(data: List<String>): Int {
         total += calculateFuel(it.toInt())
     }
     return total
-}
-
-fun getData(): List<String> {
-    val data = File("src/main/resources/inputdata.txt")
-    return data.readLines()
 }
 
 fun calculateFuel(mass: Int): Int {
--- AoC-day1/src/test/kotlin/AdventOfCodeTest.kt
+++ AoC-day1/src/test/kotlin/AdventOfCodeTest.kt
@@ -16,4 +16,19 @@ class AdventofcodeTest {
         assertThat(calculateFuel(mass1), equalTo(966))
         assertThat(calculateFuel(mass2), equalTo(50346))
     }
+
+
+    @Test
+    fun `Should test IntCode is correct after processing - Day 2`() {
+        val intCode1 = "1,0,0,0,99"
+        val intCode2 = "2,3,0,3,99"
+        val intCode3 = "2,4,4,5,99,0"
+        val intCode4 = "1,1,1,4,99,5,6,0,99"
+
+        assertThat(calculateIntCodeFinalState(intCode1), equalTo("2,0,0,0,99"))
+        assertThat(calculateIntCodeFinalState(intCode2), equalTo("2,3,0,6,99"))
+        assertThat(calculateIntCodeFinalState(intCode3), equalTo("2,4,4,5,99,9801"))
+        assertThat(calculateIntCodeFinalState(intCode4), equalTo("30,1,1,4,2,5,6,0,99"))
+    }
+
 }
