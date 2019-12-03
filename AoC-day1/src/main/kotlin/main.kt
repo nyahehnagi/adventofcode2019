6c2d14acc9e91744d414f3ddd94d5e3dddffa1e9
bromley.jones <bromley.jones@johnlewis.com>
2019-12-03 10:40:39 +0000

    day 2 completed

--- AoC-day1/src/main/kotlin/main.kt
+++ AoC-day1/src/main/kotlin/main.kt
@@ -6,8 +6,9 @@ import kotlin.math.floor
 //https://adventofcode.com/2019/day/2
 
 fun main() {
-    println(calculateTotalFuel(getData("src/main/resources/inputdata.txt")))
-    println(calculateIntCodeFinalState(getData("src/main/resources/day2input.csv")[0]))
+    //println(calculateTotalFuel(getData("src/main/resources/inputdata.txt")))
+    //println(findNounVerbToProduceGivenOutput(getData("src/main/resources/day2input.csv")[0],19690720))
+
 }
 
 fun getData(filename: String): List<String> {
@@ -15,29 +16,43 @@ fun getData(filename: String): List<String> {
     return data.readLines()
 }
 
-fun calculateIntCodeFinalState(intCode: String): String {
-    val codeList = intCode.split(",").toMutableList()
-
-    for (i in 0..codeList.lastIndex step 4) {
-        if (codeList[i] != "99") {
-            var firstValue = codeList[codeList[i + 1].toInt()].toInt()
-            var secondValue = codeList[codeList[i + 2].toInt()].toInt()
-            var thirdValue = codeList[i + 3].toInt()
-            when {
-                codeList[i].toInt() == 1 ->
-                    codeList[thirdValue] = (firstValue + secondValue).toString()
-                codeList[i].toInt() == 2 ->
-                    codeList[thirdValue] = (firstValue * secondValue).toString()
-                else -> println("Errr")
-            }
+fun findNounVerbToProduceGivenOutput (intCodeString : String, output : Int) : Pair<Int,Int>{
+    for (nounCount in 0..99){
+        for (verbCount in 0..99){
+            if (findMatchForOutput (nounCount, verbCount,output, intCodeString))
+                return Pair(nounCount,verbCount)
         }
-        println(codeList.joinToString (","))
     }
-
-    val retVal = codeList.joinToString (",")
-    return retVal
+    return Pair(0,0)
 }
 
+fun findMatchForOutput(noun : Int, verb : Int, output : Int, intCode: String): Boolean {
+    val codeList = intCode.split(",").toMutableList()
+    //Initialise noun and verb
+    codeList[1] = noun.toString()
+    codeList[2] = verb.toString()
+
+    try {
+        for (i in 0..codeList.lastIndex step 4) {
+            if (codeList[i] != "99") {
+                var firstValue = codeList[codeList[i + 1].toInt()].toInt()
+                var secondValue = codeList[codeList[i + 2].toInt()].toInt()
+                var thirdValue = codeList[i + 3].toInt()
+                when {
+                    codeList[i].toInt() == 1 ->
+                        codeList[thirdValue] = (firstValue + secondValue).toString()
+                    codeList[i].toInt() == 2 ->
+                        codeList[thirdValue] = (firstValue * secondValue).toString()
+                    else -> println("Errr")
+                }
+            }
+        }
+    }
+    catch(e : IndexOutOfBoundsException){
+        return codeList[0].toInt() == output
+    }
+    return codeList[0].toInt() == output
+}
 
 fun calculateTotalFuel(data: List<String>): Int {
     var total = 0
--- AoC-day1/src/test/kotlin/AdventOfCodeTest.kt
+++ AoC-day1/src/test/kotlin/AdventOfCodeTest.kt
@@ -9,6 +9,18 @@ import org.hamcrest.MatcherAssert.assertThat
 class AdventofcodeTest {
 
     @Test
+    fun `Should test the shortest manhatten distance for 2 wires that cross`() {
+        val wire1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+        val wire2 = "U62,R66,U55,R34,D71,R55,D58,R83"
+        val wire3 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+        val wire4 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+
+        assertThat(calculateManhattenDistance(wire1,wire2), equalTo(159))
+        assertThat(calculateManhattenDistance(wire3,wire4), equalTo(135))
+    }
+
+
+    @Test
     fun `Should test that amount of fuel calculated is correct`() {
         val mass1 = 1969
         val mass2 = 100756
@@ -17,7 +29,6 @@ class AdventofcodeTest {
         assertThat(calculateFuel(mass2), equalTo(50346))
     }
 
-
     @Test
     fun `Should test IntCode is correct after processing - Day 2`() {
         val intCode1 = "1,0,0,0,99"
@@ -25,10 +36,10 @@ class AdventofcodeTest {
         val intCode3 = "2,4,4,5,99,0"
         val intCode4 = "1,1,1,4,99,5,6,0,99"
 
-        assertThat(calculateIntCodeFinalState(intCode1), equalTo("2,0,0,0,99"))
-        assertThat(calculateIntCodeFinalState(intCode2), equalTo("2,3,0,6,99"))
-        assertThat(calculateIntCodeFinalState(intCode3), equalTo("2,4,4,5,99,9801"))
-        assertThat(calculateIntCodeFinalState(intCode4), equalTo("30,1,1,4,2,5,6,0,99"))
+        //assertThat(calculateIntCodeFinalState(intCode1), equalTo("2,0,0,0,99"))
+        //assertThat(calculateIntCodeFinalState(intCode2), equalTo("2,3,0,6,99"))
+        //assertThat(calculateIntCodeFinalState(intCode3), equalTo("2,4,4,5,99,9801"))
+        //assertThat(calculateIntCodeFinalState(intCode4), equalTo("30,1,1,4,2,5,6,0,99"))
     }
 
 }
