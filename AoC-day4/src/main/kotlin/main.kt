f82198707f43e01f227d6650f5ed75da74d3aaaf
bromley.jones <bromley.jones@johnlewis.com>
2019-12-04 09:35:13 +0000

    tidy

--- AoC-day4/src/main/kotlin/main.kt
+++ AoC-day4/src/main/kotlin/main.kt
@@ -1,2 +1,10 @@
 package AoCday4
 
+fun main(){
+    println(calculateNumberOfPossiblePassword("245318","765747"))
+}
+
+fun calculateNumberOfPossiblePassword (startNumber : String, endNumber : String) : Int {
+    val startNumberArray = startNumber.split("").toTypedArray()
+
+}
