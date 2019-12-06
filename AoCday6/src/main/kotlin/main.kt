df2d915d5501e3f20acaf10c9bacea23af4406c0
nyahehnagi <bromley.jones@gmail.com>
2019-12-06 20:49:26 +0000

    day 6 part 2 complete - somewhat buggy code which I have made work with hacks. tsk . I am getting tired and my 2 year old is crying

--- AoCday6/src/main/kotlin/main.kt
+++ AoCday6/src/main/kotlin/main.kt
@@ -5,10 +5,8 @@ import java.io.File
 
 fun main() {
     val orbitData = getData("src/main/resources/day6inputdata.txt")
-    //val orbitData = listOf<String>("COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L")
+    //val orbitData = listOf<String>("COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN")
     buildObjectMap(orbitData)
-
-
 }
 
 fun buildObjectMap(orbitData: List<String>) {
@@ -17,8 +15,7 @@ fun buildObjectMap(orbitData: List<String>) {
     spaceObjectMap.createObjectMap(orbitData)
     spaceObjectMap.countOrbits()
     println(spaceObjectMap.countOrbits())
-
-
+    println(spaceObjectMap.getShortestOrbitalTransfers("YOU", "SAN"))
 }
 
 fun getData(filename: String): List<String> {
@@ -27,11 +24,12 @@ fun getData(filename: String): List<String> {
 }
 
 class SpaceObjectMap {
+
     var universalCentreOfMass = SpaceObject("COM")
-    var allObjects : List<String> = listOf<String>()
+    var allObjects: List<String> = listOf<String>()
 
     fun createObjectMap(orbitData: List<String>) {
-        val tmpObject : MutableList<String> = mutableListOf()
+        val tmpObject: MutableList<String> = mutableListOf()
         val tmpOrbitData = orbitData.toMutableList()
 
         universalCentreOfMass.buildObjectMap(tmpOrbitData)
@@ -45,19 +43,42 @@ class SpaceObjectMap {
 
 
         }
-        //terrible idea this.. need to get my head around recursing through later on
+        //terrible idea this.. need to get my head around recursing
         allObjects = tmpObject.toList().distinctBy { it }
 
     }
 
-    fun countOrbits():Int{
-        //TODO think of a better way to go through the different objects rather than this foreach
-        var counter : Int = 0
+    fun countOrbits(): Int {
+        //TODO think of a better way to go through the different objects rather than this foreach aka traverse the tree properly
+        var counter: Int = 0
         allObjects.forEach {
             counter += universalCentreOfMass.countOrbits(it)
 
         }
-        return counter - allObjects.count()
+        return counter - allObjects.count() // this is because I have a bug in my recursion and I could all the objects
+    }
+
+    fun getShortestOrbitalTransfers(start: String, end: String): Int {
+        val startTotalOrbitsToCOM = universalCentreOfMass.getTotalOrbitsForObject(start)
+        val endTotalOrbitsToCOM = universalCentreOfMass.getTotalOrbitsForObject(end)
+
+        val tmpList: MutableList<String> = mutableListOf()
+        // There's probably a way to use maps and what not here. Need to learn more in this area
+        startTotalOrbitsToCOM.forEach {
+            if (!endTotalOrbitsToCOM.contains(it)) {
+                tmpList.add(it)
+            }
+        }
+        endTotalOrbitsToCOM.forEach {
+            if (!startTotalOrbitsToCOM.contains(it)) {
+                tmpList.add(it)
+            }
+        }
+
+        tmpList.remove(start) // I actually have 2 of these in the list.. something wrong with my recursion again
+        tmpList.remove(end) // I actually have 2 of these in the list.. something wrong with my recursion again
+
+        return tmpList.count() - 2 // minus 2 because I removed the intersecting node - horrible heh
     }
 }
 
@@ -66,8 +87,26 @@ data class SpaceObject(
     val objectDirectOrbitedBy: MutableList<SpaceObject> = mutableListOf()
 ) {
 
-    fun countOrbits(name: String) : Int{
-        if (spaceObjectName == name){
+    fun getTotalOrbitsForObject(name: String): List<String> {
+        if (spaceObjectName == name) {
+            return listOf(name)
+        }
+        // Check direct Orbits
+        if (objectDirectOrbitedBy.isNullOrEmpty()) {
+            return emptyList()
+        } else {
+            objectDirectOrbitedBy.forEach {
+                val retval = it.getTotalOrbitsForObject(name)
+                if (retval.isNotEmpty()) {
+                    return retval + listOf(it.spaceObjectName)
+                }
+            }
+        }
+        return emptyList()
+    }
+
+    fun countOrbits(name: String): Int {
+        if (spaceObjectName == name) {
             return 1
         }
         // Check direct Orbits
@@ -83,14 +122,14 @@ data class SpaceObject(
     }
 
     fun buildObjectMap(orbitData: MutableList<String>) {
-        val tmpOrbitData : MutableList<String> = orbitData.toMutableList()
+        val tmpOrbitData: MutableList<String> = orbitData.toMutableList()
 
         orbitData.forEach {
             val parts = it.split(")")
             val tmpSpaceObjectName = parts[0]
             val tmpSpaceObjectOrbitedBy = parts[1]
 
-            if (spaceObjectName == tmpSpaceObjectName){
+            if (spaceObjectName == tmpSpaceObjectName) {
                 objectDirectOrbitedBy.add(SpaceObject(tmpSpaceObjectOrbitedBy))
                 tmpOrbitData.remove(it)
             }
@@ -100,6 +139,7 @@ data class SpaceObject(
             it.buildObjectMap(tmpOrbitData)
         }
     }
+
 
 }
 
