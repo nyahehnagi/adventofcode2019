892d448ea74e5efae73ae1b69efb20cba349e776
nyahehnagi <bromley.jones@gmail.com>
2019-12-08 22:30:21 +0000

    refactor and tidy up

--- aoc-day8/src/main/kotlin/main.kt
+++ aoc-day8/src/main/kotlin/main.kt
@@ -7,93 +7,78 @@ fun getData(filename: String): List<String> {
     return data.readLines()
 }
 
-fun main(){
+fun main() {
 
-    val image  = Image(25,6, getData("src/main/resources/day8inputdata.txt")[0])
+    val image = Image(25, 6, getData("src/main/resources/day8inputdata.txt")[0])
     //val image = Image(3,2, "123456789012")
     //val image = Image(2,2,"0222112222120000")
-    println(image.getPart1())
-
-
-    val output :String = image.getPart2()
-    var index = 0
-    while (index < output.count()) {
-        println(output.substring(index, index + (25)).replace("1","X").replace("0"," "))
-        index +=  25
-    }
+    println(image.getMultiplyOfOneAndTwoDigtsOnLayerWithLeastZeroes())
+    printHumanReadableImage(image.generateVisibleLayer(), image.width)
 
 }
 
-class Layer(layerString: String){
+fun printHumanReadableImage(imageString: String, width: Int) =
+    imageString.chunked(width).map { it.replace("1", "X").replace("0", " ") }.forEach { println(it) }
 
-    val layerData : MutableList<Int> = mutableListOf()
+class Layer(layerString: String) {
 
-    init{
-        layerString.forEach {
-            layerData.add(it.toString().toInt())
-        }
+    val layerData: MutableList<Int> = mutableListOf()
+
+    init {
+        layerString.forEach { layerData.add(it.toString().toInt()) }
     }
 
-    fun digitCount(digit : Int) : Int{
+    fun digitCount(digit: Int): Int {
         return layerData.count { it == digit }
     }
 }
 
-class Image(_width : Int, _height : Int, imageString : String){
+class Image(_width: Int, _height: Int, imageString: String) {
 
-    val width : Int = _width
-    val height : Int = _height
+    val width: Int = _width
+    val height: Int = _height
 
-    val layerList : MutableList<Layer> = mutableListOf()
+    val layerList: MutableList<Layer> = mutableListOf()
 
-    init{
+    init {
         createLayers(imageString)
     }
 
-    fun createLayers (imageString : String){
-
-        var index  = 0
-
-        while (index < imageString.count()) {
-            layerList.add (Layer(imageString.substring(index, index + (width * height))))
-            index +=  width * height
-        }
+    fun createLayers(imageString: String) {
+        imageString.chunked(width * height).forEach { layerList.add(Layer(it)) }
     }
 
-    fun getLayerWithLeastNumberOfDigits(digit : Int) : Int {
+    fun getLayerWithLeastNumberOfDigits(digit: Int): Int {
 
         var minCount = layerList[0].digitCount(digit)
         var minLayer = 0
         layerList.forEachIndexed { index, layer ->
             val digitCount = layer.digitCount(digit)
-            if ( digitCount < minCount){
+            if (digitCount < minCount) {
                 minCount = digitCount
                 minLayer = index
             }
         }
-
         return minLayer
     }
 
-    fun getPart2(): String{
+    fun generateVisibleLayer(): String {
 
-        var visible : String = ""
-        for (i in 0..layerList[0].layerData.lastIndex){
-            visible += getVisibleColour(0,i).toString()
+        var visible: String = ""
+        for (i in 0..layerList[0].layerData.lastIndex) {
+            visible += getVisibleColour(0, i).toString()
         }
-
         return visible
     }
 
-
-    fun getVisibleColour(layerNumber : Int, layerIndex : Int) : Int {
-        if (layerList[layerNumber].layerData[layerIndex] == 0 || layerList[layerNumber].layerData[layerIndex] == 1)
-            return layerList[layerNumber].layerData[layerIndex]
+    fun getVisibleColour(layerNumber: Int, layerIndex: Int): Int {
+        return if (layerList[layerNumber].layerData[layerIndex] == 0 || layerList[layerNumber].layerData[layerIndex] == 1)
+            layerList[layerNumber].layerData[layerIndex]
         else
-            return getVisibleColour (layerNumber + 1, layerIndex)
+            getVisibleColour(layerNumber + 1, layerIndex)
     }
 
-    fun getPart1 (): Int{
+    fun getMultiplyOfOneAndTwoDigtsOnLayerWithLeastZeroes(): Int {
         val indexOfLayerWithFewestZeros = getLayerWithLeastNumberOfDigits(0)
         val oneCount = layerList[indexOfLayerWithFewestZeros].digitCount(1)
         val twoCount = layerList[indexOfLayerWithFewestZeros].digitCount(2)
